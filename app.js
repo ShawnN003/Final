2adedeeb846a979184b381130b82731c4d181aa4
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-02-27 13:34:53 -0800

    fixing pool

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 })
